9335efcd6556e42a3e6e4f2e61e999167bf1781d
Debdut Goswami <debdutgoswami@gmail.com>
2019-09-26 22:46:13 +0530

    Add files via upload

---
+++ day-5/setoperations.py
@@ -0,0 +1,11 @@
+s1 = {1, 2, 3 ,4, 5, 6}
+s2 = {5, 7, 9, 11}
+print('length of set 1 is',len(s1))
+s1.add(15)
+print(s1)
+s1.update(s2)
+print(s1)
+s1.remove(1)
+print(s1)
+s1.clear()
+print(s1)
---
+++ day-5/setunionintersectiondifference.py
@@ -0,0 +1,5 @@
+s1 = {1, 2, 3 ,4, 5, 6}
+s2 = {5, 7, 9, 11}
+print(s1.union(s2))
+print(s1.intersection(s2))
+print(s1.difference(s2))
